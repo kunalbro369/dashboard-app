d47ce34468b54b620e066480db079a8afb7b08e3
kunalbro369 <kkulkarni369@gmail.com>
2023-09-26 15:19:30 +0530

    first commit

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,36 @@
+import React , { createContext , useContext,
+useState} from 'react'
+
+const StateContext = createContext();
+
+const initialState = {
+    chat: false,
+    cart: false,
+    userProfile: false,
+    notification: false
+}
+ 
+export const ContextProvider = ({ children}) => {
+    const [activeMenu, setActiveMenu] = useState(true)
+    const [isClicked, setIsCliked] = useState(initialState)
+    const [screenSize, setScreenSize]= useState(undefined)
+
+    const handleClick = (clicked) => {
+        setIsCliked({...initialState,[clicked]:true});
+    }
+    return (
+        <StateContext.Provider
+        value={{ activeMenu,
+             setActiveMenu,
+              isClicked, 
+              setIsCliked, 
+              handleClick,
+              screenSize, 
+              setScreenSize}}
+        >
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = ()=> useContext(StateContext)
